a6ca080e420fb67610d4c66fe32081e8af76f608
Daffa Rihadatul Fairus <hanura423@gmail.com>
2025-03-28 01:03:18 +0700

    Layout: kasih warna pada layout utama

---
+++ src/data/data.ts
@@ -0,0 +1,17 @@
+export const Navmenu = [
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    title: "Services",
+    link: "/services",
+  },
+  {
+    id: 3,
+    title: "About Us",
+    link: "/about-us",
+  },
+];
